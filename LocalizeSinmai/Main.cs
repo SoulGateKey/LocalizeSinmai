3516a1ad97bb48c70b90bb58eed72261d9b7d3c9
SoulGateKey <soulgatekey@gmail.com>
2025-02-07 06:19:39 +0800

    Initial Commit

---
+++ LocalizeSinmai/Main.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using MelonLoader;
+using LocalizeSinmai.Fix;
+using System.Reflection;
+namespace LocalizeSinmai
+{
+    public static class BuildInfo
+    {
+        public const string Name = "LocalizeSinmai";
+        public const string Description = "Sinmai Chinese Localization";
+        public const string Author = "SoulGateKey";
+        public const string Company = null;
+        public const string Version = "1.0.0";
+        public const string DownloadLink = null;
+    }
+
+    public class LocalizeSinmai : MelonMod
+    {   
+        private static bool _hasErrors;
+        public void Patch(Type type, bool isNested = false)
+        {
+            MelonLogger.Msg($"> Patching {type}");
+            try
+            {
+                HarmonyInstance.PatchAll(type);
+                foreach (var nested in type.GetNestedTypes())
+                {
+                    Patch(nested, true);
+                }
+
+                var customMethod = type.GetMethod("DoCustomPatch", BindingFlags.Public | BindingFlags.Static);
+                customMethod?.Invoke(null, [HarmonyInstance]);
+            }
+            catch (Exception e)
+            {
+                MelonLogger.Error($"Failed to patch {type}: {e}");
+                _hasErrors = true;
+            }
+            //HarmonyLib.Harmony.CreateAndPatchAll(type);
+        }
+
+        private static void ApplyPatch()
+        {
+            //似乎仅在需要加载配置文件时需要写
+        }
+
+        public override void OnInitializeMelon()
+        {
+            MelonLogger.Msg("Loading mod settings...");
+            Patch(typeof(FontFix));
+            MelonLogger.Msg("Loaded!");
+        }
+    }
+}
---
+++ LocalizeSinmai/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle(LocalizeSinmai.BuildInfo.Name)]
+[assembly: AssemblyDescription(LocalizeSinmai.BuildInfo.Description)]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Created by " + LocalizeSinmai.BuildInfo.Author)]
+[assembly: AssemblyProduct("LocalizeSinmai")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: MelonInfo(typeof(LocalizeSinmai.LocalizeSinmai), LocalizeSinmai.BuildInfo.Name, LocalizeSinmai.BuildInfo.Version, LocalizeSinmai.BuildInfo.Author, LocalizeSinmai.BuildInfo.DownloadLink)]
+[assembly: MelonColor()]
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
+// If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
+// Values for MelonGame Attribute can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame(null, null)]
